c060b90e5377beed83333d468dafcbf5022f4f8f
Jannaten Nayem <jannaten@eduix.it>
2020-07-28 11:03:44 +0300

    New important changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,45 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   devtool: "cheap-module-source-map",
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        resolve: {
+          extensions: [".js", ".jsx"],
+        },
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\/.(png|jp(e*)g|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    watchContentBase: false,
+    open: true,
+    clientLogLevel: "silent",
+    writeToDisk: true,
+    historyApiFallback: true,
+    compress: true,
+    hot: true,
+    host: "0.0.0.0",
+    port: 9000,
+    public: `localhost:9000`,
+    proxy: {
+      "/api": {
+        target: `http://localhost:8000`,
+      },
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
